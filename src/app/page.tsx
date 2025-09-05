ba0dae95b10aed9034a26d761c89b8bb7665e624
unknown <arbaazuniquegzp@gmail.com>
2025-09-05 16:56:48 +0530

    chnages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 
 
 "use client"
-import React, { useState, useEffect } from 'react';
-import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
+import React from 'react';
 import Banner from '@/components/Home/Banner';
 import QuickFacilities from '@/components/Home/QuickFacilities';
 import AboutUsSection from '@/components/Home/AboutUsSection';
